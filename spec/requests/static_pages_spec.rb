5047b97d612f01fd98d10cb4cdb3776ef8b4ddeb
Kevin Henshall <kevin.henshall@gmail.com>
2012-12-02 10:46:52 +1100

    add contact us page and refactor rspec integration tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should contain 'Sample App'" do
       visit "/static_pages/home"
@@ -11,7 +14,7 @@ describe "StaticPages" do
     it "should contain have the right title" do
       visit "/static_pages/home"
       page.should have_selector 'title',
-        :text => "Ruby on Rails Tutorial Sample App | Home"
+        :text => "#{base_title} | Home"
     end
   end
 
@@ -25,7 +28,7 @@ describe "StaticPages" do
     it "should contain have the right title" do
       visit "/static_pages/help"
       page.should have_selector 'title',
-        :text => "Ruby on Rails Tutorial Sample App | Help"
+        :text => "#{base_title} | Help"
     end
   end
 
@@ -39,7 +42,21 @@ describe "StaticPages" do
     it "should contain have the right title" do
       visit "/static_pages/about"
       page.should have_selector 'title',
-        :text => "Ruby on Rails Tutorial Sample App | About Us"
+        :text => "#{base_title} | About Us"
+    end
+  end
+
+  describe "Contact Us page" do
+    it "should contain 'Contact Us'" do
+      visit "/static_pages/contact"
+      page.should have_selector 'h1',
+        :text => "Contact Us"
+    end
+
+    it "should contain have the right title" do
+      visit "/static_pages/contact"
+      page.should have_selector 'title',
+        :text => "#{base_title} | Contact Us"
     end
   end
 end
